edcb443b4cba3fc6b8b8d9a8663268e5fdad8107
kageru <kageru@encode.moe>
2019-12-06 10:16:50 +0100

    Shorten D6P1 in Kotlin

--- 2019/06/day06.kt
+++ 2019/06/day06.kt
@@ -1,13 +1,8 @@
 fun main() {
-    val input = generateSequence(::readLine)
-        .map { it.split(")") }
-        .groupBy { it[0] }
-        .mapValues{ it.value.map { it[1] } }
-    val rootNode = input.keys - input.values.flatten().toSet()
-    
-    println("Part 1: ${countOrbiters(input, rootNode.first(), 0)}")
+    val input = generateSequence(::readLine).map { it.split(")") }.groupBy { it[0] }.mapValues{ it.value.map { it[1] } }
+    val rootNode = (input.keys - input.values.flatten().toSet()).first()
+    println("Part 1: ${countOrbiters(input, rootNode, 0)}")
 }
 
-fun <T>countOrbiters(graph: Map<T, List<T>>, key: T, acc: Int): Int {
-    return acc + (graph[key]?.map { countOrbiters(graph, it, acc+1) }?.sum() ?: 0)
-}
+fun <T>countOrbiters(graph: Map<T, List<T>>, key: T, acc: Int): Int =
+    acc + (graph[key]?.map { countOrbiters(graph, it, acc+1) }?.sum() ?: 0)
