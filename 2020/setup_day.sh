60d2a911b9cf69c0b12a99db1935ef552e77ff54
kageru <kageru@encode.moe>
2020-12-07 16:17:07 +0100

    Extend setup day template

--- 2020/setup_day.sh
+++ 2020/setup_day.sh
@@ -12,13 +12,30 @@ xsel -b > input
 # add trailing newline if necessary
 sed -i -e '$a\' input
 
-echo 'fn read_input() -> String {
-    std::fs::read_to_string("input").unwrap()
+echo '
+#![feature(test)]
+extern crate test;
+use std::env;
+
+fn read_input() -> String {
+    std::fs::read_to_string(env::args().nth(1).unwrap_or(String::from("input"))).unwrap()
+}
+
+fn parse_input(raw: &str) -> ! {
+    unimplemented!()
+}
+
+fn main() {
+    let input = parse_input(&read_input());
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::black_box;
+
+    const TEST_INPUT: &str = "";
 
 }' >> src/main.rs
