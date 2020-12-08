bf291daefa80633f873beb052c8c012e8ed35d22
kageru <kageru@encode.moe>
2020-12-08 13:19:16 +0100

    clippy

--- 2020/src/bin/day06.rs
+++ 2020/src/bin/day06.rs
@@ -1,8 +1,6 @@
 #![feature(test)]
 extern crate test;
-use std::{
-    collections::HashSet, env, ops::{BitAnd, BitOr}
-};
+use std::{collections::HashSet, env};
 
 fn main() {
     let input = parse_input(&read_input());
@@ -11,7 +9,13 @@ fn main() {
 }
 
 fn read_input() -> String {
-    std::fs::read_to_string(env::args().nth(1).filter(|n| n != "--bench").unwrap_or(String::from("inputs/day06"))).unwrap()
+    std::fs::read_to_string(
+        env::args()
+            .nth(1)
+            .filter(|n| n != "--bench")
+            .unwrap_or(String::from("inputs/day06")),
+    )
+    .unwrap()
 }
 
 fn parse_input(raw: &str) -> Vec<Vec<HashSet<char>>> {
