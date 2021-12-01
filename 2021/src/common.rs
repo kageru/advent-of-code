63c695608ececc3f30fb34c4e5769a4d160866e4
kageru <kageru@encode.moe>
2021-12-01 13:30:10 +0100

    Remove env stuff from input reading
    No idea what last year me was thinking

--- 2021/src/common.rs
+++ 2021/src/common.rs
@@ -1,13 +1,5 @@
-use std::env;
-
 pub fn read_file(day: usize) -> String {
-    std::fs::read_to_string(
-        env::args()
-            .nth(1)
-            .filter(|n| n != "--bench")
-            .unwrap_or_else(|| format!("inputs/day{:0>2}", day)),
-    )
-    .unwrap()
+    std::fs::read_to_string(format!("inputs/day{:0>2}", day)).unwrap()
 }
 
 #[inline]
