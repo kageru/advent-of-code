b734f0c5af58ab1ab09903943f16b569c0ff74c2
kageru <kageru@encode.moe>
2021-12-01 23:12:39 +0100

    Get alternative input file via env variable instead

--- 2021/src/common.rs
+++ 2021/src/common.rs
@@ -1,13 +1,7 @@
 use std::env;
 
 pub fn read_file(day: usize) -> String {
-    std::fs::read_to_string(
-        env::args()
-            .nth(1)
-            .filter(|n| n != "--bench")
-            .unwrap_or_else(|| format!("inputs/day{:0>2}", day)),
-    )
-    .unwrap()
+    std::fs::read_to_string(env::var("AOC_INPUT").unwrap_or_else(|_| format!("inputs/day{:0>2}", day))).unwrap()
 }
 
 #[inline]
