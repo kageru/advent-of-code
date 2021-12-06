fcab7acc746672fb71417a0505d8af6f5c6d3c3b
kageru <kageru@encode.moe>
2021-12-06 13:46:34 +0100

    Slightly optimize input parsing

--- 2021/src/common.rs
+++ 2021/src/common.rs
@@ -4,16 +4,15 @@ pub fn read_file(day: usize) -> String {
     std::fs::read_to_string(env::var("AOC_INPUT").unwrap_or_else(|_| format!("inputs/day{:0>2}", day))).unwrap()
 }
 
-#[inline]
 pub fn parse_nums(l: &str) -> Vec<usize> {
     l.lines().map(parse_num).collect()
 }
 
-#[inline]
 pub fn parse_nums_comma(l: &str) -> Vec<usize> {
-    l.split(',').map(parse_num).collect()
+    l.trim().split(',').map(parse_num).collect()
 }
 
+#[inline]
 fn parse_num(s: &str) -> usize {
-    s.trim().parse().unwrap_or_else(|e| panic!("Invalid number {s}: {e:?}"))
+    s.parse().unwrap_or_else(|e| panic!("Invalid number {s}: {e:?}"))
 }
