0b91179c2958de80dd04147e6aea32f90e682759
kageru <kageru@encode.moe>
2024-12-03 12:21:30 +0100

    add 2024/03/2

--- 2024/src/bin/day03.rs
+++ 2024/src/bin/day03.rs
@@ -7,44 +7,67 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, take},
     character::complete::digit1,
-    combinator::map_res,
+    combinator::{map, map_res},
     multi::many1,
-    sequence::{delimited, preceded, separated_pair},
+    sequence::{delimited, separated_pair},
 };
 
-const DAY: usize = 03;
+const DAY: usize = 3;
 type I = i32;
-type Parsed = Vec<(I, I)>;
+type Parsed = Vec<Instruction>;
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+enum Instruction {
+    Mul(I, I),
+    Noop,
+    Do,
+    Dont,
+}
 
 fn parse_input(raw: &str) -> Parsed {
-    parse_command(raw).expect("Failed to parse").1
+    let mut instructions = parse_command(raw).expect("Failed to parse").1;
+    instructions.retain(|i| i != &Instruction::Noop);
+    instructions
+}
+
+fn digit(input: &str) -> IResult<&str, I> {
+    map_res(digit1, str::parse)(input)
 }
 
 fn parse_command(input: &str) -> IResult<&str, Parsed> {
     many1(alt((
-        preceded(
-            tag("mul"),
-            delimited(tag("("), separated_pair(map_res(digit1, str::parse::<I>), tag(","), map_res(digit1, str::parse::<I>)), tag(")")),
-        ),
-        map_res(take(1u32), |_| Ok::<_, !>((0, 0))),
+        delimited(tag("mul("), map(separated_pair(digit, tag(","), digit), |(a, b)| Instruction::Mul(a, b)), tag(")")),
+        map(tag("do()"), |_| Instruction::Do),
+        map(tag("don't()"), |_| Instruction::Dont),
+        map(take(1u32), |_| Instruction::Noop),
     )))(input)
 }
 
 fn part1(parsed: &Parsed) -> I {
-    parsed.iter().map(|(a, b)| a * b).sum()
+    parsed.iter().map(|i| if let Instruction::Mul(a, b) = i { a * b } else { 0 }).sum()
 }
 
-fn part2(parsed: &Parsed) -> usize {
-    unimplemented!()
+fn part2(parsed: &Parsed) -> I {
+    parsed
+        .iter()
+        .scan(1, |enabled, &i| {
+            if let Instruction::Mul(a, b) = i {
+                Some(*enabled * a * b)
+            } else {
+                *enabled = (i == Instruction::Do) as I;
+                Some(0)
+            }
+        })
+        .sum()
 }
 
 boilerplate! {
-    TEST_INPUT == "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    TEST_INPUT == "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
     for tests: {
         part1: { TEST_INPUT => 161 },
-        part2: { TEST_INPUT => 0 },
+        part2: { TEST_INPUT => 48 },
     },
     bench1 == 187825547,
-    bench2 == 0,
-    bench_parse: Vec::len => 11596,
+    bench2 == 85508223,
+    bench_parse: Vec::len => 782,
 }
