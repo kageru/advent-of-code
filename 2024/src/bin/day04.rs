ab357efbf1effc5841d258080d54ac6244d5fa16
kageru <kageru@encode.moe>
2024-12-04 11:44:46 +0100

    add 2024/04/2

--- 2024/src/bin/day04.rs
+++ 2024/src/bin/day04.rs
@@ -3,53 +3,52 @@ extern crate test;
 use aoc2024::{boilerplate, common::*};
 
 const DAY: usize = 4;
-type Parsed = Vec<Vec<char>>;
+type Parsed = Vec<Vec<u8>>;
 
 fn parse_input(raw: &str) -> Parsed {
-    raw.lines().map(|l| l.chars().collect()).collect()
+    raw.lines().map(|l| l.bytes().collect()).collect()
 }
 
-fn is_xmas(mut slice: Vec<&char>) -> bool {
-    let forwards = slice == [&'X', &'M', &'A', &'S'];
-    slice.reverse();
-    let backwards = slice == [&'X', &'M', &'A', &'S'];
-    forwards || backwards
+fn is_xmas(slice: &[u8; 4]) -> bool {
+    slice == &[b'X', b'M', b'A', b'S'] || slice == &[b'S', b'A', b'M', b'X']
+}
+
+fn xmas_slice<F: Fn(usize) -> Option<u8>>(f: F) -> [u8; 4] {
+    [0usize, 1, 2, 3].map(|n| f(n).unwrap_or(0))
 }
 
 fn part1(grid: &Parsed) -> usize {
-    let mut count = 0;
-    for x in 0..grid.len() {
-        for y in 0..grid[0].len() {
-            // From each position, try
-            //
-            //    /
-            //   /
-            //  /
-            // X---
-            // |\
-            // | \
-            // |  \
-            //
-            // while skipping all that are out of bounds
-            count += [
-                (0..=3).map(|n| grid.get(x + n)?.get(y.checked_sub(n)?)).collect::<Option<Vec<_>>>(),
-                (0..=3).map(|n| grid.get(x)?.get(y + n)).collect(),
-                (0..=3).map(|n| grid.get(x + n)?.get(y)).collect(),
-                (0..=3).map(|n| grid.get(x + n)?.get(y + n)).collect(),
+    (0..grid.len())
+        .flat_map(|x| (0..grid[0].len()).map(move |y| (x, y)))
+        .filter(|&(x, y)| grid[x][y] == b'X' || grid[x][y] == b'S')
+        .map(|(x, y)| {
+            // From each position X, try
+            //       /
+            //      /
+            //     /
+            //    X---
+            //    |\
+            //    | \
+            //    |  \
+            [
+                xmas_slice(|n| grid.get(x + n)?.get(y.checked_sub(n)?).copied()),
+                xmas_slice(|n| grid.get(x)?.get(y + n).copied()),
+                xmas_slice(|n| grid.get(x + n)?.get(y).copied()),
+                xmas_slice(|n| grid.get(x + n)?.get(y + n).copied()),
             ]
             .into_iter()
-            .flatten()
-            // map takes ownership (which we want) while filter doesn’t
-            .map(|s| is_xmas(s))
-            .filter(|&b| b)
-            .count();
-        }
-    }
-    count
+            .filter(is_xmas)
+            .count()
+        })
+        .sum()
 }
 
-fn part2(parsed: &Parsed) -> usize {
-    unimplemented!()
+fn is_x_mas(grid: &Parsed, (x, y): (usize, usize)) -> bool {
+    grid[x][y] == b'A' && grid[x - 1][y - 1] + grid[x + 1][y + 1] == b'M' + b'S' && grid[x + 1][y - 1] + grid[x - 1][y + 1] == b'M' + b'S'
+}
+
+fn part2(grid: &Parsed) -> usize {
+    (1..grid.len() - 1).flat_map(|x| (1..grid[0].len() - 1).map(move |y| (x, y))).filter(|&p| is_x_mas(grid, p)).count()
 }
 
 boilerplate! {
@@ -65,9 +64,9 @@ MAMMMXMMMM
 MXMXAXMASX"
     for tests: {
         part1: { TEST_INPUT => 18 },
-        part2: { TEST_INPUT => 0 },
+        part2: { TEST_INPUT => 9 },
     },
     bench1 == 2547,
-    bench2 == 0,
+    bench2 == 1939,
     bench_parse: Vec::len => 140,
 }
