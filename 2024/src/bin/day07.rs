f58bbc59dc877859384dca12198c01e45ace1a52
kageru <kageru@encode.moe>
2024-12-07 23:11:43 +0100

    make day 7 less redundant (but slower)

--- 2024/src/bin/day07.rs
+++ 2024/src/bin/day07.rs
@@ -1,72 +1,40 @@
 #![feature(test)]
 extern crate test;
 use aoc2024::{boilerplate, common::*};
+use std::ops::{Add, Mul};
 
 const DAY: usize = 7;
-type I = i64;
+type I = usize;
 type Parsed = Vec<(I, Vec<I>)>;
-
-enum Op {
-    Add,
-    Mul,
-    Concat,
-}
 
 fn parse_input(raw: &str) -> Parsed {
     raw.lines().filter_map(|l| l.split_once(": ")).map(|(r, ns)| (parse_num(r), parse_nums_separator(ns, ' '))).collect()
 }
 
+const OPS: [fn(I, I) -> I; 3] = [Add::add, Mul::mul, concat];
+
 fn concat(a: I, b: I) -> I {
-    let log = b.ilog10() + 1;
-    (a * 10i64.pow(log) as I) + b
+    (a * 10usize.pow(b.ilog10() + 1)) + b
 }
 
-fn can_be_solved_p1(target: I, sum: I, ns: &[I]) -> bool {
-    can_be_solved::<false>(target, sum, ns, Op::Add) || can_be_solved::<false>(target, sum, ns, Op::Mul)
-}
-
-fn can_be_solved_p2(target: I, sum: I, ns: &[I]) -> bool {
-    can_be_solved::<true>(target, sum, ns, Op::Add)
-        || can_be_solved::<true>(target, sum, ns, Op::Mul)
-        || can_be_solved::<true>(target, sum, ns, Op::Concat)
-}
-
-fn can_be_solved<const P2: bool>(target: I, sum: I, ns: &[I], op: Op) -> bool {
-    match (ns, op) {
-        ([n], Op::Add) => sum + n == target,
-        ([n], Op::Mul) => sum * n == target,
-        ([n], Op::Concat) => concat(sum, *n) == target,
-        ([n, ns @ ..], Op::Add) => {
-            if P2 {
-                can_be_solved_p2(target, sum + n, ns)
-            } else {
-                can_be_solved_p1(target, sum + n, ns)
-            }
-        }
-        ([n, ns @ ..], Op::Mul) => {
-            if P2 {
-                can_be_solved_p2(target, sum * n, ns)
-            } else {
-                can_be_solved_p1(target, sum * n, ns)
-            }
-        }
-        ([n, ns @ ..], Op::Concat) => {
-            if P2 {
-                can_be_solved_p2(target, concat(sum, *n), ns)
-            } else {
-                can_be_solved_p1(target, concat(sum, *n), ns)
-            }
-        }
+fn can_be_solved_inner<F: Fn(I, I) -> I, const NUM_OPS: usize>(target: I, sum: I, ns: &[I], op: F) -> bool {
+    match ns {
+        [n] => op(sum, *n) == target,
+        [n, ns @ ..] => OPS.iter().take(NUM_OPS).any(|&f| can_be_solved_inner::<_, NUM_OPS>(target, op(sum, *n), ns, f)),
         _ => unreachable!(),
     }
 }
 
+fn can_be_solved<const N: usize>(parsed: &Parsed) -> I {
+    parsed.iter().filter_map(|(r, ns)| OPS.iter().take(N).any(|&f| can_be_solved_inner::<_, N>(*r, ns[0], &ns[1..], f)).then_some(*r)).sum()
+}
+
 fn part1(parsed: &Parsed) -> I {
-    parsed.iter().filter_map(|(r, ns)| (can_be_solved_p1(*r, ns[0], &ns[1..])).then_some(*r)).sum()
+    can_be_solved::<2>(parsed)
 }
 
 fn part2(parsed: &Parsed) -> I {
-    parsed.iter().filter_map(|(r, ns)| (can_be_solved_p2(*r, ns[0], &ns[1..])).then_some(*r)).sum()
+    can_be_solved::<3>(parsed)
 }
 
 boilerplate! {
@@ -85,6 +53,7 @@ boilerplate! {
         part2: {
             TEST_INPUT => 11387,
             "156: 15 6" => 156,
+            "3267: 81 40 27" => 3267,
         },
     },
     unittests: {
