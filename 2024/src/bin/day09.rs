0bc0aa027021b0c64a20fc60ca32f7ee5c1a76cc
kageru <kageru@encode.moe>
2024-12-09 17:09:34 +0100

    more day 9 cleanup

--- 2024/src/bin/day09.rs
+++ 2024/src/bin/day09.rs
@@ -11,56 +11,47 @@ fn parse_input(raw: &str) -> Parsed {
     raw.trim_end().bytes().map(|b| (b - b'0') as I).collect()
 }
 
-fn build_disk<T, F: Fn((bool, I, I)) -> Iter, Iter: IntoIterator<Item = T>>(parsed: &Parsed, f: F) -> Vec<T> {
-    parsed
-        .iter()
-        .zip(iter::successors(Some(true), |b| Some(!b))) // alternate between true and false
-        .zip((0..).intersperse(0)) // block IDs
-        .map(|((&size, is_file), id)| (is_file, id, size))
-        .flat_map(f)
-        .collect()
+fn build_disk<T, F: Fn((I, &I)) -> Iter, Iter: IntoIterator<Item = T>>(parsed: &Parsed, f: F) -> Vec<T> {
+    (0..).intersperse(0).zip(parsed).flat_map(f).collect()
 }
 
 fn part1(parsed: &Parsed) -> I {
-    let mut disk = build_disk(parsed, |(is_file, id, size)| iter::repeat_n(is_file.then_some(id), size));
+    let mut disk = build_disk(parsed, |(id, &size)| iter::repeat_n(id, size));
     let mut from = disk.len() - 1;
-    let mut to = 0;
+    let mut to = parsed[0];
     while from > to {
         match (disk[from], disk[to]) {
-            (Some(_), None) => {
+            (1.., 0) => {
                 disk.swap(to, from);
                 from -= 1;
                 to += 1;
             }
-            (None, _) => from -= 1,
-            (_, Some(_)) => to += 1,
+            (0, _) => from -= 1,
+            (_, 1..) => to += 1,
         }
     }
-    disk.into_iter().flatten().enumerate().map(|(i, id)| i * id).sum()
+    disk.into_iter().enumerate().map(|(i, id)| i * id).sum()
 }
 
 fn part2(parsed: &Parsed) -> I {
-    let mut disk = build_disk(parsed, |(is_file, id, size)| iter::once((is_file.then_some(id), size)));
+    let mut disk = build_disk(parsed, |(id, &size)| iter::once((id, size)));
     for src_index in (2..disk.len()).rev() {
-        match disk[src_index] {
-            (None, _) => (),
-            (Some(_), src_size) => {
-                for dst_index in 1..src_index {
-                    match disk[dst_index] {
-                        (None, dst_size) if dst_size > src_size => {
-                            disk[dst_index] = (None, dst_size - src_size);
-                            disk.insert(dst_index, disk[src_index]);
-                            disk[src_index + 1] = (None, src_size);
-                        }
-                        (None, dst_size) if dst_size == src_size => disk.swap(src_index, dst_index),
-                        _ => continue,
+        if let (1.., src_size) = disk[src_index] {
+            for dst_index in 1..src_index {
+                match disk[dst_index] {
+                    (0, dst_size) if dst_size > src_size => {
+                        disk[dst_index] = (0, dst_size - src_size);
+                        disk.insert(dst_index, disk[src_index]);
+                        disk[src_index + 1] = (0, src_size);
                     }
-                    break;
+                    (0, dst_size) if dst_size == src_size => disk.swap(src_index, dst_index),
+                    _ => continue,
                 }
+                break;
             }
         }
     }
-    disk.into_iter().flat_map(|(id, size)| iter::repeat_n(id.unwrap_or(0), size)).enumerate().map(|(i, n)| i * n).sum()
+    disk.into_iter().flat_map(|(id, size)| iter::repeat_n(id, size)).enumerate().map(|(i, n)| i * n).sum()
 }
 
 boilerplate! {
