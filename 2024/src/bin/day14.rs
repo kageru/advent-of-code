3c5cbb6c8c4b603b4eb7aa727f81cd3722051527
kageru <kageru@encode.moe>
2024-12-14 19:25:05 +0100

    add 2024/14/2
    stole the tip detection idea for this one

--- 2024/src/bin/day14.rs
+++ 2024/src/bin/day14.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 extern crate test;
 use aoc2024::common::*;
+use fnv::FnvHashSet;
 use itertools::Itertools;
 use std::ops::AddAssign;
 use tuple_map::TupleMap2;
@@ -8,6 +9,7 @@ use tuple_map::TupleMap2;
 const DAY: usize = 14;
 type I = i32;
 type Parsed = Vec<((I, I), (I, I))>;
+type Robot<const X: I, const Y: I> = ((Wrapped<X>, Wrapped<Y>), (I, I));
 
 struct Wrapped<const LIMIT: I>(I);
 
@@ -29,19 +31,19 @@ fn parse_input(raw: &str) -> Parsed {
         .collect()
 }
 
+fn convert_to_wrapped<const X: I, const Y: I>(parsed: &Parsed) -> Vec<Robot<X, Y>> {
+    parsed.iter().map(|&((x, y), speeds)| ((Wrapped::<X>(x), Wrapped::<Y>(y)), speeds)).collect_vec()
+}
+
+fn step<const X: I, const Y: I>(((xpos, ypos), (xspeed, yspeed)): &mut Robot<X, Y>) {
+    *xpos += *xspeed;
+    *ypos += *yspeed;
+}
+
 fn part1<const XLIMIT: I, const YLIMIT: I>(parsed: &Parsed) -> usize {
-    let mut robots = parsed
-        .iter()
-        // .skip(10)
-        // .take(1)
-        .map(|&((x, y), speeds)| ((Wrapped::<XLIMIT>(x), Wrapped::<YLIMIT>(y)), speeds))
-        .collect_vec();
+    let mut robots = convert_to_wrapped::<XLIMIT, YLIMIT>(parsed);
     for _ in 0..100 {
-        for ((xpos, ypos), (xspeed, yspeed)) in robots.iter_mut() {
-            *xpos += *xspeed;
-            *ypos += *yspeed;
-            dbg!(xpos.0, ypos.0);
-        }
+        robots.iter_mut().for_each(step);
     }
     let mut q1 = 0;
     let mut q2 = 0;
@@ -58,22 +60,32 @@ fn part1<const XLIMIT: I, const YLIMIT: I>(parsed: &Parsed) -> usize {
             q3 += 1;
         } else if x > xhalf && y > yhalf {
             q4 += 1;
-        } else {
-            dbg!(x, y);
         }
     }
-    dbg!(q1, q2, q3, q4, robots.len());
     q1 * q2 * q3 * q4
 }
 
 fn part2(parsed: &Parsed) -> usize {
-    unimplemented!()
+    let mut robots = convert_to_wrapped::<100, 102>(parsed);
+    (1..)
+        .find(|_| {
+            robots.iter_mut().for_each(step);
+            tree_tip_exists(&robots)
+        })
+        .unwrap()
+}
+
+fn tree_tip_exists<const X: I, const Y: I>(robots: &[Robot<X, Y>]) -> bool {
+    let positions: FnvHashSet<_> = robots.iter().map(|((x, y), _)| (x.0, y.0)).collect();
+    positions
+        .iter()
+        .any(|(x, y)| (1..5).flat_map(|offset| [(x + offset, y + offset), (x - offset, y + offset)]).all(|pt| positions.contains(&pt)))
 }
 
 fn main() {
     let raw_input = read_file(DAY);
     let input = parse_input(&raw_input);
-    println!("Part 1: {}", part1::<102, 100>(&input));
+    println!("Part 1: {}", part1::<100, 102>(&input));
     println!("Part 2: {}", part2(&input));
 }
 
@@ -104,6 +116,13 @@ p=9,5 v=-3,-3";
     fn part1_bench(b: &mut test::Bencher) {
         let raw = &read_file(DAY);
         let input = parse_input(&raw);
-        b.iter(|| assert_eq!(part1::<100, 10>(test::black_box(&input)), 223020000));
+        b.iter(|| assert_eq!(part1::<100, 102>(test::black_box(&input)), 223020000));
+    }
+
+    #[bench]
+    fn part2_bench(b: &mut test::Bencher) {
+        let raw = &read_file(DAY);
+        let input = parse_input(&raw);
+        b.iter(|| assert_eq!(part2(test::black_box(&input)), 7338));
     }
 }
