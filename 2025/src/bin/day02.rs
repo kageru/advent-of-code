9fe053bcfb3a1b758a87f7255e25c4ca4bb8f2c2
kageru <kageru@encode.moe>
2025-12-02 11:47:13 +0100

    add 2025/02/02

--- 2025/src/bin/day02.rs
+++ 2025/src/bin/day02.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 extern crate test;
 use aoc2025::{boilerplate, common::*};
+use itertools::Itertools;
 
 const DAY: usize = 2;
 type I = usize;
@@ -11,10 +12,10 @@ fn parse_input(raw: &str) -> Parsed {
 }
 
 fn part1(parsed: &Parsed) -> usize {
-    parsed.iter().map(|&(start, end)| (start..=end).filter(is_invalid).sum::<I>()).sum()
+    parsed.iter().map(|&(start, end)| (start..=end).filter(|&n| is_invalid(n)).sum::<I>()).sum()
 }
 
-fn is_invalid(n: &I) -> bool {
+fn is_invalid(n: I) -> bool {
     let num_digits = n.ilog10() as usize + 1;
     if num_digits.is_odd() {
         return false;
@@ -25,16 +26,56 @@ fn is_invalid(n: &I) -> bool {
 }
 
 fn part2(parsed: &Parsed) -> usize {
-    unimplemented!()
+    parsed.iter().map(|&(start, end)| (start..=end).filter(|&n| is_invalid_p2(n)).sum::<I>()).sum()
+}
+
+fn is_invalid_p2(n: I) -> bool {
+    let s = n.to_string();
+    let b = s.as_bytes();
+    'outer: for step_size in 1..=(b.len() / 2) {
+        if b.len() % step_size != 0 {
+            continue;
+        }
+        for offset in 0..step_size {
+            if b.iter().skip(offset).step_by(step_size).dedup().count() != 1 {
+                continue 'outer;
+            }
+        }
+
+        return true;
+    }
+    false
 }
 
 boilerplate! {
     TEST_INPUT == "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
     for tests: {
         part1: { TEST_INPUT => 1227775554 },
-        part2: { TEST_INPUT => 0 },
+        part2: {
+            TEST_INPUT => 4174379265,
+            "11-22" => 33,
+            "95-115" => 210,
+            "998-1012" => 2009,
+            "1188511880-1188511890" => 1188511885,
+            "222220-222224" => 222222,
+            "1698522-1698528" => 0,
+            "446443-446449" => 446446,
+            "38593856-38593862" => 38593859,
+            "824824821-824824827" => 824824824,
+            "2121212118-2121212124" => 2121212121,
+        },
+    },
+    unittests: {
+        is_invalid_p2: {
+            999 => true,
+            1009 => false,
+            1010 => true,
+            222222 => true,
+            446446 => true,
+            38593859 => true,
+        }
     },
     bench1 == 28846518423,
-    bench2 == 0,
+    bench2 == 31578210022,
     bench_parse: Vec::len => 38,
 }
