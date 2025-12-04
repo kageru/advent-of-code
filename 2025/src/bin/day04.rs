5a84cd88fa61f1c7100b9ffdd3e4051cffdfd311
kageru <kageru@encode.moe>
2025-12-04 12:52:38 +0100

    add 2025/04/2

--- 2025/src/bin/day04.rs
+++ 2025/src/bin/day04.rs
@@ -5,7 +5,9 @@ use aoc2025::{
     boilerplate,
     common::*,
     grid::{Grid, HashGrid},
+    position::Pos,
 };
+use itertools::Itertools;
 
 const DAY: usize = 4;
 type Parsed = HashGrid<Tile, 2>;
@@ -23,14 +25,27 @@ fn parse_input(raw: &str) -> Parsed {
 }
 
 fn part1(parsed: &Parsed) -> usize {
+    find_accessible(parsed).count()
+}
+
+fn find_accessible(parsed: &Parsed) -> impl Iterator<Item = Pos<i64, 2>> {
     parsed
         .indices()
         .filter(|p| parsed.get(p) == Some(&Paper) && { p.neighbors().into_iter().filter(|np| parsed.get(np) == Some(&Paper)).count() < 4 })
-        .count()
 }
 
 fn part2(parsed: &Parsed) -> usize {
-    unimplemented!()
+    let mut modified = parsed.clone();
+    loop {
+        let accessible = find_accessible(&modified).collect_vec();
+        if accessible.is_empty() {
+            break;
+        }
+        for p in accessible {
+            *modified.get_mut(&p).unwrap() = Empty;
+        }
+    }
+    parsed.fields.values().filter(|&&t| t == Paper).count() - modified.fields.values().filter(|&&t| t == Paper).count()
 }
 
 boilerplate! {
@@ -47,9 +62,9 @@ boilerplate! {
 @.@.@@@.@."
     for tests: {
         part1: { TEST_INPUT => 13 },
-        part2: { TEST_INPUT => 0 },
+        part2: { TEST_INPUT => 43 },
     },
     bench1 == 1409,
-    bench2 == 0,
+    bench2 == 8366,
     bench_parse: aoc2025::grid::Grid::len => 19321,
 }
