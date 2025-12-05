3d609b1bbeb2b5db1a051390e5492c29e1f27315
kageru <kageru@encode.moe>
2025-12-05 14:37:00 +0100

    add 2025/05/2

--- 2025/src/bin/day05.rs
+++ 2025/src/bin/day05.rs
@@ -1,10 +1,11 @@
-#![feature(test)]
+#![feature(test, get_disjoint_mut_helpers)]
 extern crate test;
 use aoc2025::{boilerplate, common::*};
+use core::slice::GetDisjointMutIndex;
 use std::ops::RangeInclusive;
 
 const DAY: usize = 5;
-type I = i64;
+type I = usize;
 type Parsed = (Vec<RangeInclusive<I>>, Vec<I>);
 
 fn parse_input(raw: &str) -> Parsed {
@@ -17,8 +18,21 @@ fn part1((fresh, items): &Parsed) -> usize {
     items.iter().filter(|i| fresh.iter().any(|range| range.contains(i))).count()
 }
 
-fn part2(parsed: &Parsed) -> usize {
-    unimplemented!()
+fn part2((ranges, _): &Parsed) -> usize {
+    let mut ranges = ranges.clone();
+    ranges.sort_unstable_by_key(|r| *r.start());
+    let mut i = 0;
+    while i < ranges.len() {
+        let current = ranges[i].clone();
+        match ranges.iter().enumerate().skip(i + 1).find(|(_, r)| r.is_overlapping(&current)) {
+            Some((idx, range)) => {
+                ranges[i] = *current.start()..=*(current.end().max(range.end()));
+                ranges.remove(idx);
+            }
+            None => i += 1,
+        }
+    }
+    ranges.into_iter().map(|r| r.count()).sum()
 }
 
 boilerplate! {
@@ -36,9 +50,9 @@ boilerplate! {
 32"
     for tests: {
         part1: { TEST_INPUT => 3 },
-        part2: { TEST_INPUT => 0 },
+        part2: { TEST_INPUT => 14 },
     },
     bench1 == 558,
-    bench2 == 0,
+    bench2 == 344813017450467,
     bench_parse: |(a, b): &Parsed| (a.len(), b.len()) => (187, 1000),
 }
