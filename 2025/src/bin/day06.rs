e160f938dc7ee58f3f89ff91c4bfd6ba36d86ce8
kageru <kageru@encode.moe>
2025-12-06 21:45:36 +0100

    add transpose

--- 2025/src/bin/day06.rs
+++ 2025/src/bin/day06.rs
@@ -4,41 +4,39 @@ use aoc2025::{boilerplate, common::*};
 
 const DAY: usize = 6;
 type I = usize;
-type Parsed = (Vec<Op>, Vec<Vec<I>>);
+type Parsed<'a> = (&'a str, Vec<&'a str>);
 
-enum Op {
-    Add,
-    Mul,
-}
-
-fn parse_input(raw: &str) -> Parsed {
+fn parse_input(raw: &str) -> Parsed<'_> {
     let mut lines = raw.lines().rev();
-    let operators = lines
-        .next()
-        .unwrap()
-        .split_ascii_whitespace()
-        .map(|c| match c {
-            "*" => Op::Mul,
-            "+" => Op::Add,
-            _ => unreachable!(),
-        })
-        .collect();
-    let lines = lines.map(|l| l.split_ascii_whitespace().map(parse_num).collect()).collect();
-    (operators, lines)
+    (lines.next().unwrap(), lines.rev().collect())
 }
 
-fn part1((ops, nums): &Parsed) -> usize {
-    ops.iter()
-        .enumerate()
-        .map(|(i, op)| match op {
-            &Op::Add => nums.iter().map(|ns| ns[i]).sum::<I>(),
-            &Op::Mul => nums.iter().map(|ns| ns[i]).product(),
+fn part1((ops, lines): &Parsed) -> usize {
+    let nums = transpose(&lines.iter().map(|l| l.split_ascii_whitespace().map(parse_num::<I>).collect()).collect());
+    ops.split_ascii_whitespace()
+        .zip(nums)
+        .map(|(op, ns)| match op {
+            "+" => ns.iter().sum::<I>(),
+            "*" => ns.iter().product(),
+            _ => unreachable!(),
         })
         .sum()
 }
 
-fn part2(parsed: &Parsed) -> usize {
+fn part2((ops, lines): &Parsed) -> usize {
     unimplemented!()
+}
+
+fn transpose<T: Copy + Default>(v: &Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let size = v.len();
+    let inner_size = v[0].len();
+    let mut out = vec![vec![T::default(); size]; inner_size];
+    for i in 0..size {
+        for j in 0..inner_size {
+            out[j][i] = v[i][j];
+        }
+    }
+    out
 }
 
 boilerplate! {
@@ -53,5 +51,5 @@ boilerplate! {
     },
     bench1 == 4805473544166,
     bench2 == 0,
-    bench_parse: |(ops, nums): &Parsed| (ops.len(), nums.len()) => (1000, 4),
+    bench_parse: |(ops, nums): &Parsed| (ops.len(), nums.len()) => (3727, 4),
 }
