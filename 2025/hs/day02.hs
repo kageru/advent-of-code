8d9ec1c1361b76981ba4f04482acb2de5d4bd707
kageru <kageru@encode.moe>
2025-12-23 15:21:43 +0100

    finish day 2 in haskell

--- 2025/hs/day02.hs
+++ 2025/hs/day02.hs
@@ -6,7 +6,8 @@ main :: IO ()
 main = do
   input <- parse <$> getContents
   print input
-  putStrLn $ "Part 1: " ++ show (part1 input)
+  putStrLn $ "Part 1: " ++ show (solve isValid input)
+  putStrLn $ "Part 2: " ++ show (solve isValid2 input)
 
 parse :: String -> Input
 parse = map (\line -> let (a, b) = break (== '-') line in (read a, read $ tail b)) . splitOn ","
@@ -16,5 +17,12 @@ isValid n = a == b
   where s = show n
         (a, b) = splitAt (length s `div` 2) s
 
-part1 :: Input -> Int
-part1 = sum . concatMap (filter isValid . \(a, b) -> [a .. b])
+isValid2 :: Int -> Bool
+isValid2 n = any (chunksAreEqual s) [1..length s `div` 2]
+  where s = show n
+
+chunksAreEqual :: String -> Int -> Bool
+chunksAreEqual s len = all (uncurry (==)) $ zip =<< tail $ chunksOf len s
+
+solve :: (Int -> Bool) -> Input -> Int
+solve f = sum . concatMap (filter f . \(a, b) -> [a .. b])
